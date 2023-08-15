ab014d527e06df4424fa2e10e55f72e996e21375
whtsgonhre <boxofmonster@mail.ru>
2023-08-15 18:47:05 +0500

    First Commit

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
-const express = require('express')
-const path = require('path')
+import express from 'express'
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+import cors from 'cors'
+import dotenv from 'dotenv'
+import axios from 'axios'
+
+dotenv.config()
 
 const PORT = process.env.PORT || 8080
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express()
+
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
+app.use(bodyParser.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors({ origin: '*' }))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.listen(PORT)
+app.post('/email', async (req, res) => {
+    const { name, number } = req.body;
+    const message = encodeURI(`<b>Имя:</b> ${name}\n<b>Телефон:</b> <a href="tel:${number}">${number}</a>`)
+
+    try {
+        await axios.post(`https://api.telegram.org/bot${process.env.TOKEN_BOT}/sendMessage?chat_id=${process.env.CHAT_ID}&parse_mode=html&text=${message}`)
+        res.send({ reslut: true })
+    } catch(error) {
+        console.log(error)
+        res.send({ reslut: false })
+    }
+})
+
+app.listen(PORT, 'localhost', () => console.log(`http://localhost:${PORT}`))
